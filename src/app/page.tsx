d49e0d51cea037ee310a0006e427cc40a3f19f4a
Daniel Stöcklein <3005650@stud.hs-mannheim.de>
2025-02-21 21:32:40 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import { useEffect, useState } from "react";
-
 export default async function Home() {
   
-      let result = await fetch("http://127.0.0.1:5328");
+      const result = await fetch("http://127.0.0.1:5328");
       const t = await result.text()
 
 
