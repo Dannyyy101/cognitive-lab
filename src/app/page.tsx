94e8ecf7b76e2b45f6498fd109659ac655c6f6c8
Daniel Stöcklein <3005650@stud.hs-mannheim.de>
2025-02-21 21:41:01 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
-export default async function Home() {
-  
-      const result = await fetch("http://127.0.0.1:5328");
-      const t = await result.text()
+'use client'
+import { useEffect, useState } from 'react';
 
+export default function Home() {
+  const [data, setData] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await fetch("http://127.0.0.1:5328");
+        if (!result.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const text = await result.text();
+        setData(text);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="text-white">
-      {JSON.stringify(t)}
+      {JSON.stringify(data)}
     </div>
   );
 }
