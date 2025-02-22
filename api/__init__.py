864835d54aecb2916bf3c5ddd0384df1108b84bf
Daniel Stöcklein <3005650@stud.hs-mannheim.de>
2025-02-22 11:30:15 +0100

    update

--- api/__init__.py
+++ api/__init__.py
@@ -8,22 +8,20 @@ load_dotenv()
 
 
 cred = {
-  "type": os.getenv('FIREBASE_SERVICE_ACCOUNT'),
-  "project_id": os.getenv('FIREBASE_PROJECT_ID'),
-  "private_key_id": os.getenv('FIREBASE_PRIVAT_KEY_ID'),
-  "private_key": os.getenv('FIREBASE_PRIVAT_KEY'),
-  "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
-  "client_id": os.getenv('FIREBASE_CLIENT_ID'),
-  "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
-  "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
-  "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X590_CERT_URL'),
-  "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_x590_CERT_URL'),
-  "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN'),
+  "type": os.getenv('FIREBASE_SERVICE_ACCOUNT').strip('"'),
+  "project_id": os.getenv('FIREBASE_PROJECT_ID').strip('"'),
+  "private_key_id": os.getenv('FIREBASE_PRIVAT_KEY_ID').strip('"'),
+  "private_key": os.getenv('FIREBASE_PRIVAT_KEY').strip('"'),
+  "client_email": os.getenv('FIREBASE_CLIENT_EMAIL').strip('"'),
+  "client_id": os.getenv('FIREBASE_CLIENT_ID').strip('"'),
+  "auth_uri": os.getenv('FIREBASE_AUTH_URI').strip('"'),
+  "token_uri": os.getenv('FIREBASE_TOKEN_URI').strip('"'),
+  "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X590_CERT_URL').strip('"'),
+  "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_x590_CERT_URL').strip('"'),
+  "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN').strip('"'),
 }
 
-
-
-cred = credentials.Certificate('cognitive-lab-firebase-adminsdk-fbsvc-e1fede3a55.json')
+cred = credentials.Certificate(cred)
 
 app = firebase_admin.initialize_app(cred)
 
